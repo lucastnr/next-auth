76df16658e2e7f5b93190df8ce88842fa6e4b0ff
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2024-08-17 03:54:26 -0300

    feat: Add userAtom to store user data

--- app/dashboard/Header.tsx
+++ app/dashboard/Header.tsx
@@ -2,16 +2,18 @@
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import { signOut } from "../actions/auth";
+import { type User } from "@supabase/supabase-js";
+import { useHydrateAtoms } from "jotai/utils";
+import { userAtom } from "@/utils/stores";
 
-export function Header() {
+export function Header({ user }: { user: User }) {
+  useHydrateAtoms([[userAtom, user]]);
   return (
-    <div className="flex items-center w-full border-b py-3 px-4">
-      <h1 className="text-xl font-bold">Dashboard</h1>
-      <Button
-        onClick={() => signOut()}
-        className="ml-auto"
-        variant="destructive"
-      >
+    <div className="flex justify-between items-center w-full border-b py-3 px-4">
+      <p>
+        Welcome back, <span className="font-bold">{user.email}</span>
+      </p>
+      <Button onClick={() => signOut()} variant="destructive">
         <LogOut size="14" strokeWidth={3} className="mr-2" /> Logout
       </Button>
     </div>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -19,7 +19,7 @@ export default async function DashboardLayout({
 
   return (
     <div>
-      <Header />
+      <Header user={user} />
       {children}
     </div>
   );
